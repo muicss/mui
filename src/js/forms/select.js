7d1e0633f9267f0cae2d9ff340de92e057bfa06c
Andres Morey <andres@octopart.com>
2015-05-25 15:44:19 +0000

    progress on select box

--- src/js/forms/select.js
+++ src/js/forms/select.js
@@ -9,7 +9,8 @@
 var jqLite = require('../lib/jqLite.js'),
     util = require('../lib/util.js'),
     wrapperClass = 'mui-select',
-    cssSelector = '.mui-select select';
+    cssSelector = '.mui-select > select',
+    menuClass = 'mui-select-menu';
 
 
 /**
@@ -21,47 +22,134 @@ function initialize(selectEl) {
   if (selectEl._muiSelect === true) return;
   else selectEl._muiSelect = true;
 
-  // disable dropdown selector
-  jqLite.on(selectEl, 'mousedown', function(ev) {
-    ev.stopPropagation();
-    ev.preventDefault();
-  });
+  var wrapperEl = selectEl.parentNode;
 
-  jqLite.on(selectEl, 'keydown', function(ev) {
-    // spacebar, down, up
-    if (ev.keyCode === 32 || ev.keyCode === 38 || ev.keyCode === 40) {
-      ev.stopPropagation();
-      ev.preventDefault();
-    }
-  });
+  // cache element
+  wrapperEl._selectEl = selectEl;
   
-  // attach click handler
+  // handle focus
+  wrapperEl.setAttribute('tabindex', '-1');
+  jqLite.on(selectEl, 'focus', focusHandler);
+
+  // handle key presses
+  jqLite.on(wrapperEl, 'keydown', keydownHandler);
+
+  // capture clicks
+  jqLite.on(selectEl, 'mousedown', mousedownHandler);
   jqLite.on(selectEl, 'click', clickHandler);
 }
 
 
 /**
- * Handle click events on select element.
+ * Handle focus events.
+ */
+function focusHandler(ev) {
+  var selectEl = this,
+      parentEl = selectEl.parentNode,
+      origIndex = selectEl.getAttribute('tabindex');
+
+  // disable tabfocus once
+  selectEl.setAttribute('tabindex', '-1');
+  jqLite.one(parentEl, 'blur', function() {
+    if (origIndex !== null) selectEl.setAttribute('tabindex', origIndex);
+    else selectEl.removeAttribute('tabindex');
+  });
+  
+  // defer focus to parent
+  parentEl.focus();
+}
+
+
+/**
+ * Handle keydown events on wrapper element
+ * @param {Event} ev - The DOM event
+ */
+function keydownHandler(ev) {
+  // spacebar, down, up
+  if (ev.keyCode === 32 || ev.keyCode === 38 || ev.keyCode === 40) {
+    ev.preventDefault();
+
+    var selectEl = this._selectEl;
+    if (selectEl.disabled !== true) showDropdown(selectEl);
+  }
+}
+
+
+/**
+ * Disable default dropdown on mousedown
+ * @param {Event} ev - The DOM event
+ */
+function mousedownHandler(ev) {
+  if (ev.button !== 0) return;
+  ev.preventDefault();
+}
+
+
+/**
+ * Handle click events on select element
  * @param {Event} ev - The DOM event
  */
 function clickHandler(ev) {
   // only left clicks
   if (ev.button !== 0) return;
+  showDropdown(this);
+}
 
-  var selectEl = this;
 
-  // exit if disabled
-  if (selectEl.getAttribute('disabled') !== null) return;
+/**
+ * Show options dropdown.
+ * @param {Element} selectEl - The select element
+ */
+function showDropdown(selectEl) {
+  var menuEl = document.createElement('div'),
+      optionList = selectEl.children,
+      m = optionList.length,
+      optionEl,
+      itemEl,
+      i,
+      selectedPos = 0,
+      top = 13;
 
-  var menuEl = document.createElement('div');
-  jqLite.css(menuEl, {
-    position: 'absolute',
-    top: 0,
-    width: '10px',
-    height: '10px',
-    backgroundColor: 'red'
+  // build menu
+  menuEl.className = menuClass;
+  
+  // add options
+  for (i=0; i < m; i++) {
+    optionEl = optionList[i];
+    
+    itemEl = document.createElement('div');
+    itemEl.textContent = optionEl.textContent;
+    itemEl._muiPos = i;
+    
+    if (optionEl.selected) selectedPos = i;
+
+    menuEl.appendChild(itemEl);
+  }
+
+  // add selected class
+  menuEl.children[selectedPos].setAttribute('selected', true);
+  
+  // set position
+  top += selectedPos * 42;
+  jqLite.css(menuEl, 'top', '-' + top + 'px');
+
+  // attach click handler
+  jqLite.on(menuEl, 'click', function(ev) {
+    var el = ev.target,
+        pos = el._muiPos;
+
+    // ignore clicks on non-items
+    if (pos === undefined) return;
+
+    // select option
+    selectEl.children[selectedPos].removeAttribute('selected');
+    selectEl.children[pos].setAttribute('selected', true);
+    
+    // destroy menu
+    menuEl.parentNode.removeChild(menuEl);
+    selectEl.parentNode.focus();
   });
-
+  
   selectEl.parentNode.appendChild(menuEl);
 }
 
