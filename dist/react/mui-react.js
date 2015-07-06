d69c0b615ea7d42766ebdc7ad5daacdfbe24bb01
Andres Morey <andres@octopart.com>
2015-07-06 02:13:39 +0000

    cleaned up sass code

--- dist/react/mui-react.js
+++ dist/react/mui-react.js
@@ -766,8 +766,8 @@ module.exports = {
 
 (function(win) {
   // return if library has been loaded already
-  if (win._muiLoadedReact) return;
-  else win._muiLoadedReact = true;
+  if (win._muiReactLoaded) return;
+  else win._muiReactLoaded = true;
 
   // load dependencies
   var layout = require('./layout.jsx'),
