c8716403a8c3e62f85f0f246b47c86999ada2a58
Andres Morey <andres@octopart.com>
2015-12-09 10:26:10 -0500

    added react text inputs

--- src/react/forms/textinput.jsx
+++ src/react/forms/textinput.jsx
@@ -5,7 +5,185 @@
 
 'use strict';
 
-var PropTypes = React.PropTypes;
+var util = require('../../js/lib/util.js'),
+    PropTypes = React.PropTypes;
+
+
+/**
+ * Input constructor
+ * @class
+ */
+var Input = React.createClass({
+  propTypes: {
+    type: PropTypes.string,
+    value: PropTypes.string,
+    hint: PropTypes.string,
+    isAutofocus: PropTypes.bool,
+    onChange: PropTypes.func
+  },
+  getDefaultProps: function() {
+    return {
+      type: null,
+      value: '',
+      hint: null,
+      isAutofocus: false,
+      onChange: null
+    };
+  },
+  getInitialState: function() {
+    var v = this.props.value;
+
+    return {
+      value: v,
+      isDirty: Boolean(v.length)
+    };
+  },
+  render: function() {
+    var cls = {},
+        isNotEmpty = Boolean(this.state.value),
+        inputEl;
+
+    cls['mui--is-empty'] = !isNotEmpty;
+    cls['mui--is-not-empty'] = isNotEmpty;
+    cls['mui--is-dirty'] = this.state.isDirty;
+
+    cls = util.classNames(cls);
+
+    if (this.props.type === 'textarea') {
+      inputEl = (
+        <textarea
+          ref="input"
+          className={ cls }
+          rows={ this.props.rows }
+          placeholder={ this.props.hint }
+          defaultValue={ this.props.value }
+          autoFocus={ this.props.isAutofocus }
+          onChange={ this._handleChange }
+          onFocus={ this._handleFocus }
+        />
+      );
+    } else {
+      inputEl = (
+        <input
+          ref="input"
+          className={ cls }
+          type={ this.props.type }
+          value={ this.state.value }
+          placeholder={ this.props.hint }
+          autoFocus={ this.props.autofocus }
+          onChange={ this._handleChange }
+          onFocus={ this._handleFocus }
+        />
+      );
+    }
+
+    return inputEl;
+  },
+  _handleChange: function(ev) {
+    this.setState({value: ev.target.value});
+    if (this.props.onChange) this.props.onChange(ev);
+  },
+  _handleFocus: function(ev) {
+    this.setState({isDirty: true});
+  }
+});
+
+
+/**
+ * Label constructor
+ * @class
+ */
+var Label = React.createClass({
+  getDefaultProps: function() {
+    return {
+      text: '',
+      onClick: null
+    };
+  },
+  getInitialState: function() {
+    return {
+      style: {}
+    };
+  },
+  componentDidMount: function() {
+    setTimeout(function() {
+      var s = '.15s ease-out',
+          style;
+
+      style = {
+        transition: s,
+        WebkitTransition: s,
+        MozTransition: s,
+        OTransition: s,
+        msTransform: s
+      };
+
+      this.setState({
+        style: style
+      });
+    }.bind(this), 150);
+  },
+  render: function() {
+    return (
+      <label
+        refs="label"
+        style={ this.state.style }
+        onClick={ this.props.onClick }
+      >
+        { this.props.text }
+      </label>
+    );
+  }
+});
+
+
+/**
+ * TextField constructor
+ * @class
+ */
+var TextField = React.createClass({
+  propTypes: {
+    label: PropTypes.string,
+    isLabelFloating: PropTypes.bool
+  },
+  getDefaultProps: function() {
+    return {
+      label: '',
+      isLabelFloating: false
+    };
+  },
+  render: function() {
+    var cls = {},
+        labelEl;
+
+    if (this.props.label.length) {
+      labelEl = (
+        <Label
+          text={ this.props.label }
+          onClick={ this._focus }
+        />
+      );
+    }
+
+    cls['mui-textfield'] = true;
+    cls['mui-textfield--float-label'] = this.props.isLabelFloating;
+    cls = util.classNames(cls);
+
+    return (
+      <div className={ cls }>
+        <Input { ...this.props } />
+        { labelEl }
+      </div>
+    );
+  },
+  _focus: function(e) {
+    // pointer-events shim
+    if (util.supportsPointerEvents() === false) {
+      e.target.style.cursor = 'text';
+      ReactDOM.findDOMNode(this.refs.input).focus();
+    }
+  }
+});
 
 
 /**
@@ -14,19 +192,15 @@ var PropTypes = React.PropTypes;
  */
 var TextInput = React.createClass({
   propTypes: {
-    hint: PropTypes.string
+    type: PropTypes.oneOf(['text', 'email', 'password'])
   },
   getDefaultProps: function() {
     return {
-      hint: null
+      type: 'text'
     };
   },
   render: function() {
-    return (
-      <div className="mui-textfield">
-        <input type="text" placeholder={ this.props.hint } />
-      </div>
-    );
+    return (<TextField { ...this.props } />);
   }
 });
 
@@ -37,19 +211,16 @@ var TextInput = React.createClass({
  */
 var TextareaInput = React.createClass({
   propTypes: {
-    hint: PropTypes.string
+    rows: PropTypes.number
   },
   getDefaultProps: function() {
     return {
-      hint: null
+      type: 'textarea',
+      rows: 2
     };
   },
   render: function() {
-    return (
-      <div className="mui-textfield">
-        <textarea placeholder={ this.props.hint }></textarea>
-      </div>
-    );
+    return (<TextField { ...this.props } />);
   }
 });
 
