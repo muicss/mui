288b3801f2b6ddf5266684c7eb32faf764b5c5d8
gaoqi <gaoqi.liu@1001hao.com>
2016-01-23 11:57:37 +0800

    reduce code size

--- src/angular/dropdown.js
+++ src/angular/dropdown.js
@@ -14,86 +14,79 @@ angular.module('mui.dropdown', [])
         "<mui-button variant='{{variant}}' disable='disable' color='{{color}}' size='{{size}}'></mui-button>" +
         "<ul class='mui-dropdown__menu' ng-transclude></ul></div>",
 
-      compile: function(tElement, tAttrs) {
+      link: function(scope, element, attrs) {
         var dropdownClass = 'mui-dropdown',
           menuClass = 'mui-dropdown__menu',
           openClass = 'mui--is-open',
-          rightClass = 'mui-dropdown__menu--right',
-          $menu = angular.element(tElement[0].querySelector('.' + menuClass));
-          $menu.css("margin-top", '-3px');
+          rightClass = 'mui-dropdown__menu--right';
 
-        /**
-         * remove caret if nocaret attribute found
-         */
-        // !angular.isUndefined(tAttrs.nocaret) && tElement.find('mui-caret').remove();
+        scope.open = scope.open || false;
+
+        var _findMenuNode = function() {
+          return angular.element(element[0].querySelector('.' + menuClass));
+        };
+        var $menu = _findMenuNode().css("margin-top", '-3px');
+
         /**
          * menu right
          */
-        !angular.isUndefined(tAttrs.right) && $menu.addClass(rightClass);
+        !angular.isUndefined(attrs.right) && $menu.addClass(rightClass);
 
         /**
-         * return link
+         * enable html type label
          */
-        return function(scope, element, attrs) {
-          scope.open = scope.open || false;
+        attrs.$observe('label', function() {
+          var $muiButton = element.find('mui-button');
+          if (!angular.isUndefined(attrs.nocaret)) {
+            $muiButton.html(attrs.label);
+          } else {
+            $muiButton.html(attrs.label + ' <mui-caret></mui-caret>');
+          }
+          $compile($muiButton)(scope);
+        });
 
+        scope.$watch('open', function() {
+          var $menu = _findMenuNode();
+          scope.open ? $menu.addClass(openClass) : $menu.removeClass(openClass);
+        });
+
+
+        var toggleEvent = function(event) {
+          var self = element[0].contains(event.target);
           /**
-           * enable html type label
+           * [_isLink description]
+           * @return {Boolean} [description]
            */
-          attrs.$observe('label', function() {
-            var $muiButton = element.find('mui-button');
-            if (!angular.isUndefined(attrs.nocaret)) {
-              $muiButton.html(tAttrs.label);
-            } else {
-              $muiButton.html(tAttrs.label + ' <mui-caret></mui-caret>');
-            }
-            $compile($muiButton)(scope);
-          });
-
-          scope.$watch('open', function() {
-            var $menu = angular.element(element[0].querySelector('.' + menuClass));
-            scope.open ? $menu.addClass(openClass) : $menu.removeClass(openClass);
-          });
-
-
-          var toggleEvent = function(event) {
-            var self = element[0].contains(event.target),
-                $menu = angular.element(element[0].querySelector('.' + menuClass));
-            /**
-             * [_isLink description]
-             * @return {Boolean} [description]
-             */
-            var _isLink = function() {
-              var links = $menu[0].querySelectorAll('a[href]'),
-                bool = false;
-              angular.forEach(links, function(link, index) {
-                link.contains(event.target) && (bool = true);
-              });
-              return bool;
-            };
-
-            scope.$apply(function() {
-              if (_isLink() || scope.disable) {
-                return;
-              }
-              if (!self) {
-                scope.open = false;
-              } else {
-                scope.open = !scope.open;
-              }
+          var _isLink = function() {
+            var links = _findMenuNode()[0].querySelectorAll('a[href]'),
+              bool = false;
+            angular.forEach(links, function(link, index) {
+              link.contains(event.target) && (bool = true);
             });
+            return bool;
           };
 
-          /**
-           * document mousedown event
-           */
-          angular.element(document).on('mousedown', toggleEvent);
-
-          scope.$on('$destroy', function() {
-            angular.element(document).off('mousedown', toggleEvent);
+          scope.$apply(function() {
+            if (_isLink() || scope.disable) {
+              return;
+            }
+            if (!self) {
+              scope.open = false;
+            } else {
+              scope.open = !scope.open;
+            }
           });
-
         };
+
+        /**
+         * document mousedown event
+         */
+        angular.element(document).on('mousedown', toggleEvent);
+
+        scope.$on('$destroy', function() {
+          angular.element(document).off('mousedown', toggleEvent);
+        });
+
       }
     };
   });
