6ec69e2ad9d341a7ccd86ca3dd3c6b2b2f34ecb6
gaoqi <gaoqi.liu@1001hao.com>
2016-02-11 00:39:00 +0800

    remove divider-top|left|bottom|right directive

--- src/angular/divider.js
+++ src/angular/divider.js
@@ -7,36 +7,4 @@ module.exports = angular.module('mui.divider', [])
         tElement.addClass('mui-divider');
       }
     }
-  })
-  .directive('dividerTop', function() {
-    return {
-      restrict: 'A',
-      compile: function(tElement, tAttrs) {
-        tElement.addClass('mui--divider-top');
-      }
-    }
-  })
-  .directive('dividerBottom', function() {
-    return {
-      restrict: 'A',
-      compile: function(tElement, tAttrs) {
-        tElement.addClass('mui--divider-bottom');
-      }
-    }
-  })
-  .directive('dividerLeft', function() {
-    return {
-      restrict: 'A',
-      compile: function(tElement, tAttrs) {
-        tElement.addClass('mui--divider-left');
-      }
-    }
-  })
-  .directive('dividerRight', function() {
-    return {
-      restrict: 'A',
-      compile: function(tElement, tAttrs) {
-        tElement.addClass('mui--divider-right');
-      }
-    }
   });
