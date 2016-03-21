c61da72307b24739c027791511fa5693ca3d1fb8
Andres Morey <andresmarcel@gmail.com>
2016-03-20 20:14:31 -0400

    updated angular dropdown attrs

--- src/angular/dropdown.js
+++ src/angular/dropdown.js
@@ -5,45 +5,57 @@ module.exports = angular.module('mui.dropdown', [])
       transclude: true,
       replace : true,
       scope: {
-        variant: '@', //['default', 'flat', 'raised', 'fab']
-        color: '@', //['default', 'primary', 'danger', 'dark','accent']
-        size: '@', //['default', 'small', 'large']
-        open: '=?', //open ?
+        muiVariant: '@', //['default', 'flat', 'raised', 'fab']
+        muiColor: '@', //['default', 'primary', 'danger', 'dark','accent']
+        muiSize: '@', //['default', 'small', 'large']
+        muiOpen: '=?', //open ?
         disable: '=ngDisabled'
       },
       template: '<div class="mui-dropdown">' +
-                  '<mui-button variant="{{variant}}" ng-disabled="disable" color="{{color}}" '+
-                  'size="{{size}}"></mui-button>' +
-                  '<ul class="mui-dropdown__menu" ng-transclude></ul>'+
-                '</div>',
+        '<mui-button ' +
+        'mui-variant="{{muiVariant}}" ' + 
+        'mui-color="{{muiColor}}" ' +
+        'mui-size="{{muiSize}}" ' +
+        'ng-disabled="disable" ' +
+        '></mui-button>' +
+        '<ul class="mui-dropdown__menu" ng-transclude></ul>'+
+        '</div>',
       link: function(scope, element, attrs) {
         var dropdownClass = 'mui-dropdown',
-          menuClass = 'mui-dropdown__menu',
-          openClass = 'mui--is-open',
-          rightClass = 'mui-dropdown__menu--right',
-          $menu,$muiButton;
+            menuClass = 'mui-dropdown__menu',
+            openClass = 'mui--is-open',
+            rightClass = 'mui-dropdown__menu--right',
+            $menu,
+            $muiButton;
 
-        scope.open = scope.open || false;
-        if (!angular.isUndefined(attrs.disabled) && angular.isUndefined(attrs.ngDisabled)) {
+        if (!angular.isUndefined(attrs.muiOpen)) scope.open = true;
+        else scope.open = false;
+
+        if (!angular.isUndefined(attrs.disabled)
+            && angular.isUndefined(attrs.ngDisabled)) {
           scope.disable = true;
         }
 
         var _findMenuNode = function() {
           return angular.element(element[0].querySelector('.' + menuClass));
         };
+
         $menu = _findMenuNode().css('margin-top', '-3px');
 
-        //menu right
-        !angular.isUndefined(attrs.right) && $menu.addClass(rightClass);
+        // menu right
+        !angular.isUndefined(attrs.muiRightAlign)
+          && $menu.addClass(rightClass);
 
-        //html类型的 label
-        attrs.$observe('label', function() {
+        // html类型的 label
+        attrs.$observe('muiLabel', function() {
           $muiButton = angular.element(element[0].querySelector('.mui-btn'));
-          if (!angular.isUndefined(attrs.nocaret)) {
-            $muiButton.html(attrs.label);
+
+          if (!angular.isUndefined(attrs.muiNoCaret)) {
+            $muiButton.html(attrs.muiLabel);
           } else {
-            $muiButton.html(attrs.label + ' <mui-caret></mui-caret>');
+            $muiButton.html(attrs.muiLabel + ' <mui-caret></mui-caret>');
           }
+
           $compile($muiButton.children())(scope);
         });
 
@@ -61,22 +73,20 @@ module.exports = angular.module('mui.dropdown', [])
            */
           var _isLink = function() {
             var links = _findMenuNode()[0].querySelectorAll('a[href]'),
-              bool = false;
+                bool = false;
+
             angular.forEach(links, function(link, index) {
               link.contains(event.target) && (bool = true);
             });
+
             return bool;
           };
 
           scope.$apply(function() {
-            if (_isLink() || scope.disable) {
-              return;
-            }
-            if (!self) {
-              scope.open = false;
-            } else {
-              scope.open = !scope.open;
-            }
+            if (_isLink() || scope.disable) return;
+
+            if (!self) scope.open = false;
+            else scope.open = !scope.open;
           });
         };
 
@@ -88,7 +98,6 @@ module.exports = angular.module('mui.dropdown', [])
         scope.$on('$destroy', function() {
           angular.element(document).off('mousedown', toggleEvent);
         });
-
       }
     };
   });
