dd26b126d53de6229b43ab8536f8ba8801068203
pazams <maor@pazams.com>
2016-06-13 01:33:33 +0300

    integrate waves ripple lib

--- src/js/ripple.js
+++ src/js/ripple.js
@@ -8,81 +8,52 @@
 
 var jqLite = require('./lib/jqLite'),
     util = require('./lib/util'),
-    btnClass = 'mui-btn',
+    waves = require('./lib/waves'),
     btnFABClass = 'mui-btn--fab',
-    rippleClass = 'mui-ripple-effect',
-    animationName = 'mui-btn-inserted';
+    mui2wavesAddTuples = [
+        ['mui-btn--fab',['waves-circle','waves-float']],
+        ['mui-btn--raised',['waves-float']],
+        ['mui-btn--primary',['waves-light']],
+        ['mui-btn--danger',['waves-light']],
+        ['mui-btn--dark',['waves-light']],
+        ['mui-btn--accent',['waves-light']]
+    ],
+    mui2wavesSubtractTuples = [
+        ['mui-btn--flat',['waves-light']]
+    ],
+    btnClass = 'mui-btn';
 
+function getWavesEffects(el){
+    var effects = [];
 
-/**
- * Add ripple effects to button element.
- * @param {Element} buttonEl - The button element.
- */
-function initialize(buttonEl) {
-  // check flag
-  if (buttonEl._muiRipple === true) return;
-  else buttonEl._muiRipple = true;
+    mui2wavesAddTuples.forEach(function(tuple){
+        var muiClass = tuple[0],
+            wavesEffects = tuple[1];
 
-  // exit if element is INPUT (doesn't support absolute positioned children)
-  if (buttonEl.tagName === 'INPUT') return;
+        if (jqLite.hasClass(el, muiClass)){ 
+            wavesEffects.forEach(function(wavesEffect){
+                effects.push(wavesEffect);
+            });
+        }
+    
+    });
 
-  // attach event handler
-  jqLite.on(buttonEl, 'touchstart', eventHandler);
-  jqLite.on(buttonEl, 'mousedown', eventHandler);
-}
+    mui2wavesSubtractTuples.forEach(function(tuple){
+        var muiClass = tuple[0],
+            wavesEffects = tuple[1];
 
+        if (jqLite.hasClass(el, muiClass)){ 
+            wavesEffects.forEach(function(wavesEffect){
+                var effectIndex = effects.indexOf(wavesEffect);
+                if(effectIndex > -1){
+                    effects.splice(effectIndex,1);
+                }
+            });
+        }
+    
+    });
 
-/**
- * Event handler
- * @param {Event} ev - The DOM event
- */
-function eventHandler(ev) {
-  // only left clicks
-  if (ev.button !== 0) return;
-
-  var buttonEl = this;
-
-  // exit if button is disabled
-  if (buttonEl.disabled === true) return;
-
-  // de-dupe touchstart and mousedown with 100msec flag
-  if (buttonEl.touchFlag === true) {
-    return;
-  } else {
-    buttonEl.touchFlag = true;
-    setTimeout(function() {
-      buttonEl.touchFlag = false;
-    }, 100);
-  }
-
-  var rippleEl = document.createElement('div');
-  rippleEl.className = rippleClass;
-
-  var offset = jqLite.offset(buttonEl),
-      xPos = ev.pageX - offset.left,
-      yPos = ev.pageY - offset.top,
-      diameter,
-      radius;
-
-  // get height
-  if (jqLite.hasClass(buttonEl, btnFABClass)) diameter = offset.height / 2;
-  else diameter = offset.height;
-
-  radius = diameter / 2;
-  
-  jqLite.css(rippleEl, {
-    height: diameter + 'px',
-    width: diameter + 'px',
-    top: yPos - radius + 'px',
-    left: xPos - radius + 'px'
-  });
-
-  buttonEl.appendChild(rippleEl);
-  
-  window.setTimeout(function() {
-    var parentNode = rippleEl.parentNode;
-    if (parentNode) parentNode.removeChild(rippleEl);
-  }, 2000);
+    return effects;
 }
 
 
@@ -94,11 +65,18 @@ module.exports = {
 
     // markup elements available when method is called
     var elList = doc.getElementsByClassName(btnClass);
-    for (var i=elList.length - 1; i >= 0; i--) initialize(elList[i]);
+    for (var i=elList.length - 1; i >= 0; i--) {
+        var el = elList[i];
+		waves.attach(el,getWavesEffects(el));
+	} 
+    waves.init();
 
     // listen for new elements
     util.onNodeInserted(function(el) {
-      if (jqLite.hasClass(el, btnClass)) initialize(el);
+      if (jqLite.hasClass(el, btnClass)){
+		  waves.attach(el,getWavesEffects(el));
+		  waves.init();
+      }
     });
   }
 };
