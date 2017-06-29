9ea48ba9390ef6c6d811495cf10ce6a342ee04ac
Andres Morey <andresmarcel@gmail.com>
2017-06-29 12:50:15 +0000

    upgraded to jsdom 11.0.0

--- test/angular-tests/angular-helper.js
+++ test/angular-tests/angular-helper.js
@@ -1,25 +1,21 @@
+delete require.cache[require.resolve('angular')];
+delete require.cache[require.resolve('angular/angular')];
+delete require.cache[require.resolve('angular-mocks')];
+
 global.window.mocha = {}; //angular-mocks.js line 2287.
 global.window.beforeEach = beforeEach;
 global.window.afterEach = afterEach;
 global.Node = window.Node; //angular need
-
-// Since angular and angular-mocks are both singletons created once with one 
-// window-object and mocha doesn't reload modules from node_modules on watch
-// mode we'll have to invalidate the cached singletons manually.
-delete require.cache[require.resolve('angular')];
-delete require.cache[require.resolve('angular/angular')];
-delete require.cache[require.resolve('angular-mocks')];
 
 require('angular/angular');
 require('angular-mocks');
 
 global.angular = window.angular;
 
-// import jqLite
 let jqLite = require('../../src/js/lib/jqLite');
 
 module.exports = {
-  inject: window.angular.mock.inject,
-  module: window.angular.mock.module,
+  inject: angular.mock.inject,
+  module: angular.mock.module,
   jqLite: jqLite
 };
--- test/before-compilers.js
+++ test/before-compilers.js
@@ -1,7 +1,3 @@
-// monkey patch react so it thinks `window` and `document` are available
+// monkey patch so tests think `window` and `document` are available
 // globally
-var jsdom = require('jsdom');
-
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = document.defaultView;
-global.navigator = global.window.navigator;
+require('jsdom-global')();
