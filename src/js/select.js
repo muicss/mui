b349b9cb9219bd1531a8415ba08a5649755b1f5c
Andres Morey <andresmarcel@gmail.com>
2017-12-16 08:47:37 +0000

    Added  event to CSS/JS Select component (fixes https://github.com/muicss/mui/issues/257#issuecomment-352034642)

--- src/js/select.js
+++ src/js/select.js
@@ -406,8 +406,9 @@ Menu.prototype.selectCurrent = function() {
   if (this.currentPos !== this.origPos) {
     this.selectEl.selectedIndex = this.itemArray[this.currentPos]._muiIndex;
 
-    // trigger change event
+    // trigger change and input events
     util.dispatchEvent(this.selectEl, 'change', true, false);
+    util.dispatchEvent(this.selectEl, 'input', true, false);
   }
 }
 
